80e9858403ebb3255a3288764eb9fd4e2b8c7755
fish <essenchang@cathayholdings.com.tw>
2019-03-29 15:06:56 +0800

    use shell

--- cloc.sh
+++ cloc.sh
@@ -1,2 +1,3 @@
 #!/bin/sh -l
-cloc . --xml -report-file cloc.xml --exclude-dir=build,libs,assets,res --include-lang=Java,Kotlin
+echo "FFFF"
+#cloc . --xml -report-file cloc.xml --exclude-dir=build,libs,assets,res --include-lang=Java,Kotlin
